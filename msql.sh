dc32a6dff5aca2aaf41da267a6a77b613c6c365d
EC2 Default User <ec2-user@ip-172-26-13-214.ec2.internal>
2024-09-18 18:54:51 +0000

    com2

--- msql.sh
+++ msql.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-#To install MySQL on Centos 9, use this command:
-sudo yum install mariadb-server -y
+#To install MySQL:
+sudo dnf install mariadb105 -y #for amazon linux 2
+sudo yum install mariadb -y
 
 #Start the mysql service with:
 sudo systemctl start mariadb
 
 #Now enable it with:
 sudo systemctl enable mariadb
+
+#Now secure the installation by setting the root password:
+sudo mysql_secure_installation
+
